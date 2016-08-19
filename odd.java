1d0e67536d193d02b75c733ecb719e460026671b
niyaseng <niyasprof@gmail.com>
2016-08-19 10:18:00 +0530

    Add files via upload

---
+++ odd.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+class odd
+{
+public static void main(String args[])
+{ 
+Scanner s=new Scanner(System.in);
+System.out.println("Enter the number : ");
+int number=s.nextInt();
+if(number<0)
+System.out.println("negative");
+else
+System.out.println("positive");
+}
+}
+
